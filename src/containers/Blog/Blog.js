c8e2abc5cd2b434b55f83728f31a5dc6f7eaafd2
Oliver Heward <oliverhewardbie@gmail.com>
2019-07-08 15:48:02 +0100

    Added more indepth routing, including adding a dynamically nested route

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 // import axios from 'axios';
-import { Route, Link } from 'react-router-dom';
+import { Route, NavLink, Switch } from 'react-router-dom';
 import Posts from './Posts/Posts';
 import NewPost from './NewPost/NewPost';
 
@@ -15,17 +15,25 @@ class Blog extends Component {
                 <header>
                     <nav>
                         <ul>
-                            <li><Link to="/">Home</Link></li>
-                            <li><Link to={{
+                            <li><NavLink 
+                                    to="/posts/" 
+                                    exact
+                                    activeClassName="my-active"
+                                    activeStyle={{
+                                        color: '#fa923f'
+                                    }}>Posts</NavLink></li>
+                            <li><NavLink to={{
                                 pathname: '/new-post',
                                 hash: '#submit',
                                 search: '?quick-submit=true'
-                            }}>New Post</Link></li>
+                            }}>New Post</NavLink></li>
                         </ul>
                     </nav>
                 </header>
-                <Route path="/" exact component={Posts} />
-                <Route path="/new-post" component={NewPost} />
+                <Switch>
+                    <Route path="/new-post" component={NewPost} />
+                    <Route path="/posts" component={Posts} />
+                </Switch>
             </div>
         );
     }
--- src/containers/Blog/Posts/Posts.js
+++ src/containers/Blog/Posts/Posts.js
@@ -1,6 +1,8 @@
 import React , { Component } from 'react';
 import axios from '../../../axios';
+import { Route } from 'react-router-dom';
 import Post from '../../../components/Post/Post';
+import FullPost from '../FullPost/FullPost';
 import './Posts.css';
 
 class Posts extends Component {
@@ -24,29 +26,32 @@ class Posts extends Component {
             console.log(error);
         });
     }
-
+    
     postSelectedHandler = (id) => {
-        this.setState({
-            selectedPostId: id
-        });
+        this.props.history.push({pathname: '/posts/' + id});
     }
 
     render() {
         let posts = <p style={{textAlign: 'center'}}>Something went wrong!</p>
         if (!this.state.error) {
         posts = this.state.posts.map(post => {
-            return <Post 
-                        key={post.id}
-                        title={post.title}
-                        author={post.author}
-                        {...this.props}
-                        clicked={() => this.postSelectedHandler(post.id)} />;
-        });
-    }
+            return ( //<Link to={'/' + post.id} key={post.id}>
+                        <Post 
+                            title={post.title}
+                            author={post.author}
+                            key={post.id}
+                            clicked={() => this.postSelectedHandler(post.id)} />
+                    //</Link>
+                );
+            });
+        }
         return(
-            <section className="Posts">
-                {posts}
-            </section>
+            <div>
+                <section className="Posts">
+                    {posts}
+                </section>
+                <Route path={this.props.match.url + '/:id'} exact component={FullPost}/> 
+            </div>
         );
     }
 }
