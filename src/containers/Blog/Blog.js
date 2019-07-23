cb07e166d215614ab309640933297c5609e92075
Oliver Heward <oliverhewardbie@gmail.com>
2019-07-23 12:31:59 +0100

    added an async component that willl only render a component when its called

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -2,9 +2,13 @@ import React, { Component } from 'react';
 // import axios from 'axios';
 import { Route, NavLink, Switch, Redirect } from 'react-router-dom';
 import Posts from './Posts/Posts';
-import NewPost from './NewPost/NewPost';
-
+// import NewPost from './NewPost/NewPost';
 import './Blog.css';
+import asyncComponent from '../../hoc/asyncComponent';
+
+const AsyncNewPost = asyncComponent(() => {
+    return import('./NewPost/NewPost');
+});
 
 class Blog extends Component {
     state = {
@@ -15,7 +19,7 @@ class Blog extends Component {
         let guard = null;
 
         if (this.state.auth) {
-            guard = <Route path="/new-post" component={NewPost} />;
+            guard = <Route path="/new-post" component={AsyncNewPost} />;
         }
         
         return (
@@ -39,8 +43,9 @@ class Blog extends Component {
                     </nav>
                 </header>
                 <Switch>
-                    {guard}
+                    {this.state.auth ? <Route path="/new-post" component={AsyncNewPost} /> : null}
                     <Route path="/posts" component={Posts} />
+                    <Route render={() => <h1>Not Found</h1>}/>
                     <Redirect from="/" to="/posts" />
                 </Switch>
             </div>
